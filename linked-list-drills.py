eaa31ffedbbddab75d3c53833cda5115d9b6f922
Rachel Reilly <r.a.reill18@gmail.com>
2021-01-27 16:57:16 -0600

    ll class working with drills file

---
+++ linked-list-drills.py
@@ -0,0 +1,9 @@
+from ll import LinkedList
+
+test = LinkedList()
+
+test.__insert_first__(test)
+
+test.__insert_first__(test)
+
+print(test)
---
+++ ll.py
@@ -0,0 +1,44 @@
+class _Node:
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __insert_first__(item, self):
+        if self.head == None:
+            self.head = _Node(item, None)
+        else:
+            hold = self.head
+            self.head = _Node(item, hold)
+
+    def __insert_last__(item, self):
+        if self.head == None:
+            insert_first(item, None)
+        else:
+            temp_node = self.head
+            while temp_node.next != None:
+                temp_node = temp_node.next
+            temp_node.next = _Node(item, None)
+
+    def __find__(self, pos):
+        node = self.head
+        for p in pos:
+            if node.next == None:
+                return node
+            else:
+                node = node.next
+        return pos
+
+    def __insert_at__(self, item, pos):
+        node = find(pos)
+        if node.next == None:
+            node.next = _Node(item, None)
+        else:
+            hold = node.next
+            new_node = _Node(item, hold)
+            node.next = new_node
+            new_node.next = hold
