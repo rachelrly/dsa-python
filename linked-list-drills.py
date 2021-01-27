6143000f1180f98df1944dc3a21027d3149cb8dd
Rachel Reilly <r.a.reill18@gmail.com>
2021-01-27 17:22:39 -0600

    fixed bug in __insert_at__

--- linked-list-drills.py
+++ linked-list-drills.py
@@ -2,9 +2,13 @@ from ll import LinkedList
 
 test = LinkedList()
 
-test.__insert_first__('testing')
+test.__insert_first__('testing1')
 
-test.__insert_first__('testing22')
+test.__insert_last__('testing2')
+test.__insert_last__('testing3')
+test.__insert_last__('testing4')
+test.__insert_last__('testing5')
 
-d = test.__show_ll__()
-print(d)
+test.__insert_at__('testing3NEW', 0)
+
+test.__show_ll__()
--- ll.py
+++ ll.py
@@ -15,9 +15,9 @@ class LinkedList:
             hold = self.head
             self.head = _Node(item, hold)
 
-    def __insert_last__(item, self):
+    def __insert_last__(self, item):
         if self.head == None:
-            insert_first(item, None)
+            self.insert_first(item)
         else:
             temp_node = self.head
             while temp_node.next != None:
@@ -26,26 +26,28 @@ class LinkedList:
 
     def __find__(self, pos):
         node = self.head
-        for p in pos:
+        for p in range(0, pos):
             if node.next == None:
                 return node
             else:
                 node = node.next
-        return pos
+        return node
 
     def __insert_at__(self, item, pos):
-        node = find(pos)
-        if node.next == None:
-            node.next = _Node(item, None)
+        if pos == 0: self.__insert_first__(item)
         else:
-            hold = node.next
-            new_node = _Node(item, hold)
-            node.next = new_node
-            new_node.next = hold
+            node = self.__find__(pos - 1)
+            if node.next == None:
+                node.next = _Node(item, None)
+            else:
+                hold = node.next
+                new_node = _Node(item, hold)
+                node.next = new_node
+                new_node.next = hold
 
     def __show_ll__(self):
         node = self.head
         while node.next != None:
-            print(f'Value: {node.value} node.next: {node.next.value}')
+            print(f'Value: {node.value} Next: {node.next.value}')
             node = node.next
         print(f'Value: {node.value}')
